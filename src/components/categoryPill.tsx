54feea036750b998dbfe13968da674b4b38d7e45
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-10-21 14:48:33 -0500

    make category pill link context aware

--- src/components/categoryPill.tsx
+++ src/components/categoryPill.tsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
-import CategoryPillStyles from "./styleSets/categoryPillStyles";
+import { classes } from "@/utils";
 import type { CategoryType } from "@/types/CategoryTypes";
 
 export function CategoryPill(props: {
   category: CategoryType;
   dashboard: boolean;
 }) {
+  let href = props.dashboard
+    ? `/dashboard/categories/${props.category.slug}`
+    : `/categories/${props.category.slug}`;
   return (
     <Link
-      className={CategoryPillStyles.spanClasses}
-      href={`/categories/${props.category.slug}`}
+      className={classes([
+        "align-self-end",
+        "border-color-cyan-semi-transparent",
+        "border-rounded-16",
+        "border-style-solid",
+        "border-width-1",
+        "display-inline-block",
+        "hover-decoration-none",
+        "squeeze-16",
+        "squish-8",
+        "surface-char-semi-transparent",
+        "text-color-turquoise",
+        "text-small",
+      ])}
+      href={href}
       key={props.category.id}
     >
       {props.category.name}
